9dbc46efe3538842e96e1c3f2c329c4d35b5a8d3
abatar1 <evgenyhalzov@gmail.com>
2016-11-03 19:53:20 +0600

    draft

---
+++ Week6/ExpressionExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Week6
+{
+    public static class ExpressionDifferentiateExtension
+    {
+        private static Expression AddDifferentiateRule(Expression exp)
+        {
+            if (exp.NodeType == ExpressionType.Constant)
+            {
+               return Expression.Empty();
+            }
+            else if (exp.NodeType == ExpressionType.Parameter)
+            {
+                return Expression.Constant(1);
+            }
+            else
+            {
+                var a = Expression.Lambda<Func<double, double>>(exp);
+                return Expression.Lambda<Func<double, double>>(exp as Expression<Func<double, double>>).Differentiate();
+            }
+        }
+
+        public static Expression<Func<double, double>> Differentiate(this Expression<Func<double, double>> expression)
+        {
+            var expBody = expression.Body;
+            var expBin = (BinaryExpression)expBody;
+
+            if (expBody.NodeType == ExpressionType.Add)
+            {
+                var x = Expression.Parameter(typeof(int), "x");               
+
+                var leftPart = AddDifferentiateRule(expBin.Left);
+                var RightPart = AddDifferentiateRule(expBin.Right);
+
+                Expression newExpression = Expression.Lambda<Func<double, double>>(
+                         Expression.Add(leftPart, RightPart),
+                         x);
+            }
+            else if (expBody.NodeType == ExpressionType.Multiply)
+            {
+                
+            }
+            else if (expBody.NodeType == ExpressionType.Divide)
+            {
+
+            }
+            else if (expBody.NodeType == ExpressionType.Subtract)
+            {
+
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Week6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Week6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Expression<Func<double, double>> f = x => (x + x + 2 + 5);
+            Expression<Func<double, double>> df = f.Differentiate();
+
+            Console.WriteLine("f  = {0}", f);
+            Console.WriteLine("df = {0}", df);
+
+            Func<double, double> compiled = df.Compile();
+            double result = compiled.Invoke(12);
+            Console.WriteLine(result);
+        }
+    }
+}
