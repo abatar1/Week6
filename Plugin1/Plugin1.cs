86ec811e88e1032fc361e5ee737e206827b40655
abatar1 <evgenyhalzov@gmail.com>
2016-11-04 23:11:13 +0600

    first task update 1.2

--- Plugin1/Plugin1.cs
+++ Plugin1/Plugin1.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Framework;
-
 namespace Plugin1
 {
     public class Plugin1 : IPlugin
--- Week6/Program.cs
+++ Week6/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq.Expressions;
-
-namespace Week6
+﻿namespace Week6
 {
     public class Program
     {
